d994cf02c07a0cff16b5d08d0fc97ffa31dd7f5b
AntonChetveryk <chetverykanton92@gmail.com>
2020-10-26 22:02:52 +0200

    task2

--- src/components/Pages/TaskThree/TaskThree.js
+++ src/components/Pages/TaskThree/TaskThree.js
@@ -2,15 +2,60 @@ import React from 'react';
 
 import data from './data';
 
-function getResult(data) {
+function findSplitPoint() {
   const arr = [...data.set.sort((a, b) => a - b)];
-  console.log(arr);
+  let leftSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    leftSum += arr[i];
+
+    let rightSum = 0;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      rightSum += arr[j];
+    }
+
+    if (leftSum === rightSum) return i + 1;
+  }
+
+  return false;
 }
 
-getResult(data);
+const splitPoint = findSplitPoint();
+
+function getResult() {
+  let arr = [...data.set.sort((a, b) => a - b)];
+
+  if (splitPoint) {
+    let leftArr = arr.slice(0, splitPoint);
+    let rightArr = arr.slice(splitPoint);
+
+    return [leftArr, rightArr];
+  }
+
+  arr = [...data.set.sort((a, b) => b - a)];
+  let leftArr = [];
+  let rightArr = [];
+
+  for (const num of arr) {
+    const leftArrSum = leftArr.reduce((sum, cur) => sum + cur, 0);
+    const rightArrSum = rightArr.reduce((sum, cur) => sum + cur, 0);
+
+    leftArrSum <= rightArrSum ? leftArr.push(num) : rightArr.push(num);
+  }
+
+  return [leftArr, rightArr];
+}
+
+const result = getResult();
 
 function TaskThree() {
-  return <div className='container'>Task3</div>;
+  return (
+    <div className='container'>
+      <div>{`Left array: ${result[0].toString()}`}</div>
+      <div>{`Right array: ${result[1].toString()}`}</div>
+    </div>
+  );
 }
 
 export default TaskThree;
